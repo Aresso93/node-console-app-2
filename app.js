62dec741d54726352629d288e019479e9ce992fd
Aresso93 <ares.fiumicelli@gmail.com>
2023-07-29 18:27:09 +0200

    commit post ChatGPT

--- app.js
+++ app.js
@@ -1,100 +1,75 @@
 const fs = require('fs');
 
 const inputUrl = process.argv[2];
-
 const outputUrl = process.argv[3];
 
 let data = readFile(inputUrl);
 
 if (data) {
-    
-    const result = transFormData(data)
+  const rows = data.split(/\r?\n/);
+  const result = transFormData(rows);
 
-    writeData(outputUrl, result);
+  writeData(outputUrl, result);
 }
 
-//console.log('riuscito');
-
-function readFile(url){
-
-    try {
-        const data = fs.readFileSync(url, 'utf8');
-        return data;
-      } catch (err) {
-        console.error(err.message);
-        return null;
-      }
-
+function readFile(url) {
+  try {
+    const data = fs.readFileSync(url, 'utf8');
+    return data;
+  } catch (err) {
+    console.error(err.message);
+    return null;
+  }
 }
 
-function writeData(url, data){
-
-    try {
-        fs.writeFileSync(url, data);
-      } catch (err) {
-        console.error(err.message);
-      }
-
+function writeData(url, data) {
+  try {
+    fs.writeFileSync(url, data);
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
-function transFormData(rows, headerArray){
+function transFormData(rows) {
+  const header = rows.shift();
+  console.log('header: ' + header + ' ' + typeof header);
 
-    const rows = data.split(/\r?\n/);
-    const header = rows.shift();
-    
-    //console.log('header: ' + header + ' ' + typeof header); 
+  const headerArray = header.split(',');
+  console.log('headerArray: ' + headerArray + ' ' + typeof headerArray);
 
-    const headerArray = header.split(',');
-    //console.log('headerArray: ' + headerArray + ' ' + typeof headerArray);
+  let students = [];
 
-    let students = [];
-    
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    const rowArray = row.split(',');
 
-    for (let i = 0; i < rows.length; i++) {
-      const row = rows[i];
+    let student = {};
 
-      const rowArray = row.split(',');
+    console.log('rowArray: ' + rowArray[1] + ' ' + typeof rowArray);
 
-      let student = {};
+    for (let j = 0; j < headerArray.length; j++) {
+      const property = headerArray[j];
+      const value = rowArray[j];
 
-      console.log('rowArray: ' + rowArray[1] + ' ' + typeof rowArray);
-     
-      for (let j = 0; j < headerArray.length; j++) {
-        const property = headerArray[j];
-
-        const value = rowArray[j];
-
-        student[property] = value;
-
-        }
-
-        students.push(student);
-    
-        
-      }
-
-        return JSON.stringify(students);
-
+      student[property] = value;
     }
 
-    
-    
-     //4) ciclate sull'array rows;
-        //4a) create una costante rowArray splittando la singola row sulle virgole;
-        //4b) create un oggetto vuoto chiamato student;
-        //4c) ciclate sull'headerArray;
-            //4c1) per ogni elemento dell'headerArray aggiungo una proprietà all'oggetto student
-                // student[headerArray[j]] = rowArray[j];
-        //4d) aggiungo student a students
-    //5) ritorno JSON.stringify di students
+    students.push(student);
+  }
+  console.log(students);
+  return JSON.stringify(students);
+  
+}
+
+
 
 
     //A1)tipizzare i valori nel json
     //A2)aggiungere un parametro alla applicazione che mi permette di indicare il carattere divisorio
     //A3)gestire la possibilità che nel csv ci siano degli spazi non voluti 
 
-    console.log('rows: ' + rows);
-    return JSON.stringify(rows);
+    //console.log('rows: ' + rows);
+
 
     
 
@@ -112,3 +87,14 @@ function transFormData(rows, headerArray){
     //FATTO
 
     //3) create un array temporaneo chiamato students (vuoto);
+
+    //FATTO
+
+    //4) ciclate sull'array rows;
+        //4a) create una costante rowArray splittando la singola row sulle virgole;
+        //4b) create un oggetto vuoto chiamato student;
+        //4c) ciclate sull'headerArray;
+            //4c1) per ogni elemento dell'headerArray aggiungo una proprietà all'oggetto student
+                // student[headerArray[j]] = rowArray[j];
+        //4d) aggiungo student a students
+    //5) ritorno JSON.stringify di students
